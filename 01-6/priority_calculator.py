5527a750ab767f87e15e121e4b16dd78aa1bebd4
김예주 <yejoo03105@gmail.com>
2025-07-17 19:28:51 +0900

    01-6 코드 작성

--- 01-6/priority_calculator.py
+++ 01-6/priority_calculator.py
@@ -1,22 +1,3 @@
-# 입력 받은 문자들을 숫자로 변환
-def to_float(s):
-    numbers = []
-
-    # 예외처리) 아무것도 입력하지 않았을때
-    if not s:
-        print("Invalid input.")
-        return None
-
-    # 예외처리) 숫자가 아닌 값을 입력했을때
-    for num in s:
-        try:
-            number = float(num)
-            numbers.append(number)
-        except ValueError:
-            print("Invalid input.")
-            return None
-    return numbers
-
 def parse_expression(s):
     parsed = s.split()
 
