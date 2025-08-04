2a1d6af7870e1de721a298bc37266bf4b6dde29e
yejoo0310 <yejoo0310@naver.com>
2025-08-04 19:01:04 +0900

    feat: 컴퓨터 이름 표시 기능

---
+++ 01_common/03/app.py
@@ -0,0 +1,22 @@
+import socket
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+
+@app.route("/")
+def home():
+    if app.debug:
+        hostname = '컴퓨터(인스턴스) : ' + socket.gethostname()
+    else:
+        hostname = ' '
+    return render_template("index.html", computername=hostname)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 80, debug=True)
