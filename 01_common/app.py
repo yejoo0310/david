486156399f1b0324241e06165254031d1b4d2c6c
yejoo0310 <yejoo0310@naver.com>
2025-08-04 15:49:25 +0900

    Feat: 메뉴 화면추가
    /menu 라우트 추가 및 메뉴 페이지 버튼 3개 작성
    
    Resolves: #1

---
+++ 01_common/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 80)
