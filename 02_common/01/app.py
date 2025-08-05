4dbf04a013fa6de891284fef86a626af36e5c907
yejoo0310 <yejoo0310@naver.com>
2025-08-05 14:23:17 +0900

    first

--- 02_common/01/app.py
+++ 02_common/01/app.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/test1")
-def test1():
-    return render_template('test1.html')
-
 
 @app.route("/menu")
 def menu():
