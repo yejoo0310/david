99e33d8e962bc0db1f4840321c47e9851e286fc1
yejoo0310 <142380013+yejoo0310@users.noreply.github.com>
2025-08-05 14:14:00 +0900

    Update app.py

--- 02_common/01/app.py
+++ 02_common/01/app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/test1")
+def test1():
+    return render_template('test1.html')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 80)
