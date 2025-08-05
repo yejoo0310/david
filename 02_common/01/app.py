552c37e104c348e4752f2997e7434c4d219fbf2d
yejoo0310 <yejoo0310@naver.com>
2025-08-05 13:25:13 +0900

    delete test2

--- 02_common/01/app.py
+++ 02_common/01/app.py
@@ -12,9 +12,5 @@ def menu():
 def test1():
     return render_template('test1.html')
 
-@app.route("/test2")
-def test2():
-    return render_template('test2.html')
-
 if __name__ == '__main__':
     app.run('0.0.0.0', 80)
