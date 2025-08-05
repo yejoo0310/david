13a0b5b10ec8e601f791915428e3d721d559d24a
yejoo0310 <yejoo0310@naver.com>
2025-08-05 14:29:41 +0900

    first

--- 02_common/01/app.py
+++ 02_common/01/app.py
@@ -8,13 +8,5 @@ def menu():
     return render_template("menu.html")
 
 
-@app.route("/test1")
-def test1():
-    return render_template('test1.html')
-@app.route("/test2")
-def test2():
-    return render_template('test2.html')
-
-
 if __name__ == '__main__':
     app.run('0.0.0.0', 80)
