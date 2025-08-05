b73a84b05641b0508f0540fd0c079cadcdede366
yejoo0310 <142380013+yejoo0310@users.noreply.github.com>
2025-08-05 14:46:35 +0900

    Update app.py

--- 02_common/01/app.py
+++ 02_common/01/app.py
@@ -8,5 +8,10 @@ def menu():
     return render_template("menu.html")
 
 
+@app.route("/test2")
+def test2():
+    return render_template('test2.html')
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', 80)
