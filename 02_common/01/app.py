108443b20dacd15169e38fcfcf43ee8d74308a01
yejoo0310 <142380013+yejoo0310@users.noreply.github.com>
2025-08-05 14:12:14 +0900

    1

--- 02_common/01/app.py
+++ 02_common/01/app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def menu():
     return render_template("menu.html")
 
+@app.route("/test4")
+def test4():
+    return render_template('test4.html')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 80)
